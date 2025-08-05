38d031674047905a63495566c3a05a3d97da202d
Fahadbhati182 <fahadbhati3600@gamil.com>
2025-08-05 20:26:00 +0530

    Adding new url of deployed version

--- server/server.js
+++ server/server.js
@@ -47,7 +47,7 @@ app.set("trust proxy", 1); //?That lets Express recognize req.secure even behind
 app.use(express.json());
 app.use(cookieParser());
 app.use(urlencoded({ extended: true }));
-app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
+app.use(cors({ origin: ["https://connectup-freelancer-platform-2.onrender.com"], credentials: true }));
 
 app.use("/api/user", userRouter);
 app.use("/api/gig", gigRouter);
